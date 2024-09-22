502d1c73376f84caa001e22bdaa69174223cb7e9
PashaDem <dem-pasha@lenta.ru>
2024-09-22 15:53:06 +0300

    [NP-3] добавил документацию для фронтенда

--- src/nopay/settings.py
+++ src/nopay/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "corsheaders",
     "rest_framework.authtoken",
+    "drf_spectacular",
     *APPS,
 ]
 
@@ -100,5 +101,6 @@ REST_FRAMEWORK = {
         # 'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
