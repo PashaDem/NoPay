2dfb50382d8e11bad5c824f43dae71259d5d4370
PashaDem <dem-pasha@lenta.ru>
2024-10-20 22:05:27 +0300

    [NP-14] add OCR parsing using tesseract

--- src/file_service/parse_qrcode.py
+++ src/file_service/parse_qrcode.py
@@ -1,3 +1,5 @@
+import os
+import re
 from base64 import b64decode
 from datetime import datetime
 
@@ -13,6 +15,7 @@ from .minio_factory import MinioFileRepository
 logger = get_task_logger(__name__)
 
 User = get_user_model()
+PARSING_PATTERN = r"Рег\.знак:.*?\)\."
 
 
 def parse_qrcode(filename: str, user_id: int):
@@ -50,8 +53,37 @@ def parse_qrcode_task(filename: str, user_id: int) -> None:
         logger.error(err)
         return
 
-    QRCode.objects.create(**qrcode_payload, created_by=User.objects.get(id=user_id))
+    # parse text from ticket image
+    import pyocr
+    import pyocr.builders
+
+    tools = pyocr.get_available_tools()
+    tool = tools[0]  # using tesseract
+
+    img = Image.open(local_filename)
+
+    text_from_image = tool.image_to_string(
+        img, builder=pyocr.builders.TextBuilder(), lang="rus"
+    )
+    cleaned_text = "".join(text_from_image.splitlines())
+
+    match = re.search(PARSING_PATTERN, cleaned_text)
+    if not match:
+        logger.error("Не удалось распознать регистрационный знак.")
+        return
+
+    qrcode_payload.update(
+        {
+            "registration_sign": match.group(),
+            "created_by": User.objects.get(id=user_id),
+        }
+    )
+
+    QRCode.objects.create(**qrcode_payload)
     repo.remove_file_from_blob(settings.MINIO_BUCKET_NAME, filename)
+
+    if os.path.exists(local_filename):
+        os.remove(local_filename)
 
 
 def parse_qrcode_content(content: str) -> dict:
@@ -81,5 +113,4 @@ def parse_qrcode_content(content: str) -> dict:
         "payment_time": payment_time,
         "ticket_id": ticket_id,
         "qr_token": decoded_content,
-        "registration_sign": "",
     }
